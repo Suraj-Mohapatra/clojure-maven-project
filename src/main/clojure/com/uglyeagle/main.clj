1b44134e568ec62e8f88b41270f177110409d487
Suraj-Mohapatra <suraj2895@gmail.com>
2025-10-11 03:53:10 +0530

    no msg

--- src/main/clojure/com/uglyeagle/main.clj
+++ src/main/clojure/com/uglyeagle/main.clj
@@ -1,13 +1,11 @@
 (ns com.uglyeagle.main
   (:gen-class))
-  
-(defn unmain []
-  (println "This is the unmain function."))
 
+(defn unmain [] ;; this has to appear earlier than main which I am also figuring out why this bullshit :gen-class
+  (println "This is the unmain function."))
 
 (defn -main
   "this is the main function that runs when the program starts."
   [& args]
   (println "Hello, World!")
   (unmain))
-
